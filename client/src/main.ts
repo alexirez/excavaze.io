3fd75f61ee25f8982d104a8cb3c9cad6fa0d761c
Alexander <alexander89162@gmail.com>
2026-05-22 20:40:48 -0700

    added basic scene with rectangle

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,1 +1,10 @@
-console.log('client running')
+import Phaser from 'phaser'
+import { GameScene } from './scenes/GameScene'
+
+new Phaser.Game({
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#1a1a2e',
+  scene: [GameScene],
+})
