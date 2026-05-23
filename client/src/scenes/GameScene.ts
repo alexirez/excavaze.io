70065d44f8980c127eb18eeb9225d3b0814946ab
Alexander <alexander89162@gmail.com>
2026-05-22 20:53:15 -0700

    Replaced rectangle with circle, added barrel and made it aim towards cursor

--- client/src/scenes/GameScene.ts
+++ client/src/scenes/GameScene.ts
@@ -1,7 +1,7 @@
 import Phaser from 'phaser'
 
 export class GameScene extends Phaser.Scene {
-  private square!: Phaser.GameObjects.Rectangle
+  private container!: Phaser.GameObjects.Container
   private keys!: Record<string, Phaser.Input.Keyboard.Key>
 
   constructor() {
@@ -9,7 +9,10 @@ export class GameScene extends Phaser.Scene {
   }
 
   create() {
-    this.square = this.add.rectangle(400, 300, 50, 50, 0x00ff99)
+    const circle = this.add.circle(0, 0, 25, 0x00ff99)
+    const barrel = this.add.rectangle(35, 0, 40, 14, 0x00cc77)
+
+    this.container = this.add.container(400, 300, [barrel, circle])
 
     this.keys = {
       W: this.input.keyboard!.addKey('W'),
@@ -22,9 +25,16 @@ export class GameScene extends Phaser.Scene {
   update() {
     const speed = 3
 
-    if (this.keys.W.isDown) this.square.y -= speed
-    if (this.keys.S.isDown) this.square.y += speed
-    if (this.keys.A.isDown) this.square.x -= speed
-    if (this.keys.D.isDown) this.square.x += speed
+    if (this.keys.W.isDown) this.container.y -= speed
+    if (this.keys.S.isDown) this.container.y += speed
+    if (this.keys.A.isDown) this.container.x -= speed
+    if (this.keys.D.isDown) this.container.x += speed
+
+    const pointer = this.input.activePointer
+    const angle = Phaser.Math.Angle.Between(
+      this.container.x, this.container.y,
+      pointer.x, pointer.y
+    )
+    this.container.rotation = angle
   }
 }
