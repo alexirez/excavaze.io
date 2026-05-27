5d316be39d01acae0337e1ede0dd25639a05c7e2
Alexander <alexander89162@gmail.com>
2026-05-26 23:19:04 -0700

    Added square entity and corresponding type

--- server/src/entities.ts
+++ server/src/entities.ts
@@ -6,3 +6,12 @@ export interface ServerPlayer {
   state: PlayerState
   input: { dx: number; dy: number; rotation: number }
 }
+
+export interface ServerSquare {
+  id: string
+  x: number
+  y: number
+  vx: number   // current drift velocity
+  vy: number
+  angle: number  // current wander angle (radians)
+}
