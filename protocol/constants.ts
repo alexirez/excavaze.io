0c5c44100e358b5467dfb453e2f46fe9af3f6a92
Alexander <alexander89162@gmail.com>
2026-05-28 12:49:41 -0700

    Changed out of bounds background color

--- protocol/constants.ts
+++ protocol/constants.ts
@@ -3,4 +3,4 @@ export const WORLD_HEIGHT = 2000
 export const WORLD_PADDING = 200
 
 export const COLOR_BACKGROUND = 0x444444
-export const COLOR_OUTER_BOUNDS = 0xeeeeee
+export const COLOR_OUTER_BOUNDS = 0x544e4e
