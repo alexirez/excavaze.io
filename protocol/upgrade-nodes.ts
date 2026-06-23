ebd5a6b1a302093cad64dbb1601792be93862bb6
Alexander <alexander89162@gmail.com>
2026-06-23 16:03:43 -0700

    Sample tech tree done, ready for testing. Will expand once working

--- protocol/upgrade-nodes.ts
+++ protocol/upgrade-nodes.ts
@@ -8,4 +8,50 @@ export interface UpgradeNode {
     y: number
 }
 
-export const UPGRADE_NODES: UpgradeNode[] = []
+export const UPGRADE_NODES: UpgradeNode[] = [
+    {
+        id: 'drill_dmg_1',
+        desc: '+1% drill damage.',
+        icon: 'gem.svg',
+        cost: [{ currency: 'gem', amount: 30 }],
+        parents: [],
+        x: 200,
+        y: 300,
+    },
+    {
+        id: 'drill_dmg_2',
+        desc: '+2% drill damage.',
+        icon: 'gem.svg',
+        cost: [{ currency: 'gem', amount: 120 }],
+        parents: ['drill_dmg_1'],
+        x: 200,
+        y: 500,
+    },
+    {
+        id: 'drill_dmg_3',
+        desc: '+3% drill damage.',
+        icon: 'gem.svg',
+        cost: [{ currency: 'gem', amount: 450 }],
+        parents: ['drill_dmg_2'],
+        x: 200,
+        y: 700,
+    },
+    {
+        id: 'drill_dmg_4',
+        desc: '+4% drill damage.',
+        icon: 'gem.svg',
+        cost: [{ currency: 'gem', amount: 1080 }],
+        parents: ['drill_dmg_3'],
+        x: 200,
+        y: 900,
+    },
+    {
+        id: 'drill_dmg_5',
+        desc: '+5% drill damage.',
+        icon: 'gem.svg',
+        cost: [{ currency: 'gem', amount: 1600 }],
+        parents: ['drill_dmg_4'],
+        x: 200,
+        y: 1100,
+    },
+]
