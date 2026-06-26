0d00ee062ead41ceab7d33dfc992f0ddadd48906
Alexander <alexander89162@gmail.com>
2026-06-25 20:46:49 -0700

    Replaced upgrade nodes array with map for optimal usage by server

--- protocol/data/upgrade-nodes.ts
+++ protocol/data/upgrade-nodes.ts
@@ -1,3 +1,6 @@
+import { PLAYER_BASE_HP } from "../constants"
+import { PlayerState } from "../types"
+
 export interface UpgradeNode {
     id: string
     desc: string
@@ -7,165 +10,145 @@ export interface UpgradeNode {
     y: number
 }
 
-export const UPGRADE_NODES: UpgradeNode[] = [
-    {
-        id: 'drill_dmg_1',
+export const UPGRADE_NODES: Map<string, Omit<UpgradeNode, 'id'>> = new Map([
+    ['drill_dmg_1', {
         desc: '+1% drill damage',
         cost: [{ currency: 'gem', amount: 30 }],
         parents: [],
-        x: 200,
-        y: 300,
-    },
-    {
-        id: 'drill_dmg_2',
+        x: 200, y: 300,
+        apply: (state: PlayerState) => { state.drillDmgMultiplier += 0.01 }
+    }],
+    ['drill_dmg_2', {
         desc: '+2% drill damage',
         cost: [{ currency: 'gem', amount: 120 }],
         parents: ['drill_dmg_1'],
-        x: 340,
-        y: 340,
-    },
-    {
-        id: 'drill_dmg_3',
+        x: 340, y: 340,
+        apply: (state: PlayerState) => { state.drillDmgMultiplier += 0.01 }
+    }],
+    ['drill_dmg_3', {
         desc: '+3% drill damage',
         cost: [{ currency: 'gem', amount: 450 }],
         parents: ['drill_dmg_2'],
-        x: 480,
-        y: 380,
-    },
-    {
-        id: 'drill_dmg_4',
+        x: 480, y: 380,
+        apply: (state: PlayerState) => { state.drillDmgMultiplier += 0.01 }
+    }],
+    ['drill_dmg_4', {
         desc: '+4% drill damage',
         cost: [{ currency: 'gem', amount: 1080 }],
         parents: ['drill_dmg_3'],
-        x: 620,
-        y: 420,
-    },
-    {
-        id: 'drill_dmg_5',
+        x: 620, y: 420,
+        apply: (state: PlayerState) => { state.drillDmgMultiplier += 0.01 }
+    }],
+    ['drill_dmg_5', {
         desc: '+5% drill damage',
         cost: [{ currency: 'gem', amount: 1600 }],
         parents: ['drill_dmg_4'],
-        x: 760,
-        y: 460,
-    },
-    {
-        id: 'HP_1',
+        x: 760, y: 460,
+        apply: (state: PlayerState) => { state.drillDmgMultiplier += 0.01 }
+    }],
+    ['HP_1', {
         desc: '+1% HP',
         cost: [{ currency: 'gem', amount: 30 }],
         parents: [],
-        x: 1000,
-        y: 300,
-    },
-    {
-        id: 'HP_2',
+        x: 1000, y: 300,
+        apply: (state: PlayerState) => { state.maxHp += PLAYER_BASE_HP * 0.01 }
+    }],
+    ['HP_2', {
         desc: '+2% HP',
         cost: [{ currency: 'gem', amount: 120 }],
         parents: ['HP_1'],
-        x: 1140,
-        y: 340,
-    },
-    {
-        id: 'HP_3',
+        x: 1140, y: 340,
+        apply: (state: PlayerState) => { state.maxHp += PLAYER_BASE_HP * 0.01 }
+    }],
+    ['HP_3', {
         desc: '+3% HP',
         cost: [{ currency: 'gem', amount: 450 }],
         parents: ['HP_2'],
-        x: 1280,
-        y: 380,
-    },
-    {
-        id: 'HP_4',
+        x: 1280, y: 380,
+        apply: (state: PlayerState) => { state.maxHp += PLAYER_BASE_HP * 0.01 }
+    }],
+    ['HP_4', {
         desc: '+4% HP',
         cost: [{ currency: 'gem', amount: 1080 }],
         parents: ['HP_3'],
-        x: 1420,
-        y: 420,
-    },
-    {
-        id: 'HP_5',
+        x: 1420, y: 420,
+        apply: (state: PlayerState) => { state.maxHp += PLAYER_BASE_HP * 0.01 }
+    }],
+    ['HP_5', {
         desc: '+5% HP',
         cost: [{ currency: 'gem', amount: 1600 }],
         parents: ['HP_4'],
-        x: 1560,
-        y: 460,
-    },
-    {
-        id: 'drill_length_1',
+        x: 1560, y: 460,
+        apply: (state: PlayerState) => { state.maxHp += PLAYER_BASE_HP * 0.01 }
+    }],
+    ['drill_length_1', {
         desc: '+1% drill length',
         cost: [{ currency: 'gem', amount: 30 }],
         parents: [],
-        x: 200,
-        y: 600,
-    },
-    {
-        id: 'drill_length_2',
+        x: 200, y: 600,
+        apply: (state: PlayerState) => { state.drillLengthMultiplier += 0.01 }
+    }],
+    ['drill_length_2', {
         desc: '+2% drill length',
         cost: [{ currency: 'gem', amount: 120 }],
         parents: ['drill_length_1'],
-        x: 340,
-        y: 640,
-    },
-    {
-        id: 'drill_length_3',
+        x: 340, y: 640,
+        apply: (state: PlayerState) => { state.drillLengthMultiplier += 0.01 }
+    }],
+    ['drill_length_3', {
         desc: '+3% drill length',
         cost: [{ currency: 'gem', amount: 450 }],
         parents: ['drill_length_2'],
-        x: 480,
-        y: 680,
-    },
-    {
-        id: 'drill_length_4',
+        x: 480, y: 680,
+        apply: (state: PlayerState) => { state.drillLengthMultiplier += 0.01 }
+    }],
+    ['drill_length_4', {
         desc: '+4% drill length',
         cost: [{ currency: 'gem', amount: 1080 }],
         parents: ['drill_length_3'],
-        x: 620,
-        y: 720,
-    },
-    {
-        id: 'drill_length_5',
+        x: 620, y: 720,
+        apply: (state: PlayerState) => { state.drillLengthMultiplier += 0.01 }
+    }],
+    ['drill_length_5', {
         desc: '+5% drill length',
         cost: [{ currency: 'gem', amount: 1600 }],
         parents: ['drill_length_4'],
-        x: 760,
-        y: 760,
-    },
-    {
-        id: 'movement_1',
+        x: 760, y: 760,
+        apply: (state: PlayerState) => { state.drillLengthMultiplier += 0.01 }
+    }],
+    ['movement_1', {
         desc: '+1% movement',
         cost: [{ currency: 'gem', amount: 30 }],
         parents: [],
-        x: 1000,
-        y: 600,
-    },
-    {
-        id: 'movement_2',
+        x: 1000, y: 600,
+        apply: (state: PlayerState) => { state.moveSpeedMultiplier += 0.01 }
+    }],
+    ['movement_2', {
         desc: '+2% movement',
         cost: [{ currency: 'gem', amount: 120 }],
         parents: ['movement_1'],
-        x: 1140,
-        y: 640,
-    },
-    {
-        id: 'movement_3',
+        x: 1140, y: 640,
+        apply: (state: PlayerState) => { state.moveSpeedMultiplier += 0.01 }
+    }],
+    ['movement_3', {
         desc: '+3% movement',
         cost: [{ currency: 'gem', amount: 450 }],
         parents: ['movement_2'],
-        x: 1280,
-        y: 680,
-    },
-    {
-        id: 'movement_4',
+        x: 1280, y: 680,
+        apply: (state: PlayerState) => { state.moveSpeedMultiplier += 0.01 }
+    }],
+    ['movement_4', {
         desc: '+4% movement',
         cost: [{ currency: 'gem', amount: 1080 }],
         parents: ['movement_3'],
-        x: 1420,
-        y: 720,
-    },
-    {
-        id: 'movement_5',
+        x: 1420, y: 720,
+        apply: (state: PlayerState) => { state.moveSpeedMultiplier += 0.01 }
+    }],
+    ['movement_5', {
         desc: '+5% movement',
         cost: [{ currency: 'gem', amount: 1600 }],
         parents: ['movement_4'],
-        x: 1560,
-        y: 760,
-    },
-]
+        x: 1560, y: 760,
+        apply: (state: PlayerState) => { state.moveSpeedMultiplier += 0.01 }
+    }],
+])
